121c578dee343ccdb8df9337123253ecd6e351da
candacewilson <candaceyw@gmail.com>
2020-10-18 12:42:03 -0400

    added alert change

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -12,7 +12,7 @@ const Form = ({ addReg }) => {
 	const [state, setState] = useState('');
 
 	const handleSubmit = () => {
-		alert(`${firstName} You have been registered!`);
+		alert(`${firstName}, you have been registered!`);
 
 		addReg({
 			firstName,
